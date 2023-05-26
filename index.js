b7dd63ddf117418b634a2305f6f1c31c9a6b39c7
Hyunkim <zhyunz123@gmial.com>
2023-05-27 03:37:01 +0900

    seceret key add

--- index.js
+++ index.js
@@ -2,13 +2,35 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+const bodyparser = require("body-parser");
+const config = require("./config/keys");
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 mongoose
-  .connect("mongodb+srv://zhyunz123:root@cluster0.jtpve7r.mongodb.net/?retryWrites=true&w=majority")
+  .connect(config.MongoURI)
   .then(() => console.log("mongo conect"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  //user모델에 정보가 저장됨
+  //실패 시, 실패한 정보를 보내줌
+  user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      return res.json({ success: false, err });
+    });
 });
 
 app.listen(port, () => {
